bded22d7889c89489540df63b576f33b19658e0e
thermitegod <thermitethegod@gmail.com>
2022-01-22 19:25:33 -0700

    import vendor/ztd/

--- clang-format.sh
+++ clang-format.sh
@@ -1,2 +1,10 @@
-#!/bin/sh
-find ./src -iname *.h -o -iname *.c -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+#!/bin/bash
+
+# src
+find ./src -maxdepth 1 -type f -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+find ./src/ptk -maxdepth 1 -type f -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+find ./src/vfs -maxdepth 1 -type f -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+find ./src/mime-type -maxdepth 1 -type f -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
+
+# vendor
+# find ./src/vendor/ztd -maxdepth 1 -type f -o -iname *.hxx -o -iname *.cxx -o -iname *.ixx | xargs clang-format -i
